767b94ba874cb5adad6de21620d9b1456708cbea
Roman-Ojha <razzroman98@gmail.com>
2022-10-20 23:07:08 +0545

    feat(setting): showing change in range using javascript

--- scripts/settings.ts
+++ scripts/settings.ts
@@ -1,3 +1,15 @@
-console.log("hello");
+const speedValueElm = document.getElementById(
+  "speed-value"
+) as HTMLOutputElement;
+const speedInputElm = document.getElementById(
+  "setting-speed-range"
+) as HTMLInputElement;
+
+speedValueElm.innerText = speedInputElm.value;
+
+speedInputElm.addEventListener("input", (e) => {
+  const value = (e.target as HTMLInputElement).value;
+  speedValueElm.innerText = value;
+});
 
 export {};
