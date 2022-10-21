a6fcc648c3ebac7bee6e5ae211bf45cda883265d
Roman-Ojha <razzroman98@gmail.com>
2022-10-21 08:59:08 +0545

    feat: shown dynamic scaleType in homePage

--- scripts/index.ts
+++ scripts/index.ts
@@ -1,7 +1,9 @@
-import { useSpeed, useScales, scaleType } from "./state";
+import { useSpeed, useScales, useScaleType } from "./state";
 
 const scaleElm = document.getElementById("scale") as HTMLHeadingElement;
-const scaleType = document.getElementById("scale-type") as HTMLHeadingElement;
+const scaleTypeElm = document.getElementById(
+  "scale-type"
+) as HTMLHeadingElement;
 const scaleAccidental = document.getElementById(
   "accidentals"
 ) as HTMLHeadElement;
@@ -9,10 +11,13 @@ const startStopButton = document.getElementById(
   "start-stop"
 ) as HTMLButtonElement;
 const [getScales] = useScales();
-const types = ["Major", "Minor"];
+const [getScaleType] = useScaleType();
 const accidentals = ["", "#", "♭"];
 const [speed] = useSpeed();
 scaleElm.innerText = getScales().filter((scale) => scale.status)[0].name;
+scaleTypeElm.innerText = getScaleType().filter(
+  (scaleType) => scaleType.status
+)[0].name;
 
 const chooseRandomScale = (): {
   scale: string;
@@ -20,9 +25,14 @@ const chooseRandomScale = (): {
   accidentals: string;
 } => {
   const allowedScale = getScales().filter((scale) => scale.status);
+  const allowedScaleType = getScaleType().filter(
+    (scaleType) => scaleType.status
+  );
+  console.log(allowedScaleType);
   return {
     scale: allowedScale[Math.floor(Math.random() * allowedScale.length)].name,
-    type: types[Math.floor(Math.random() * types.length)],
+    type: allowedScaleType[Math.floor(Math.random() * allowedScaleType.length)]
+      .name,
     accidentals: accidentals[Math.floor(Math.random() * accidentals.length)],
   };
 };
@@ -31,7 +41,7 @@ const changeScaleDom = (): void => {
   const getRandomScale = chooseRandomScale();
   scaleElm.innerText = getRandomScale.scale;
   scaleAccidental.innerText = getRandomScale.accidentals;
-  scaleType.innerText = getRandomScale.type;
+  scaleTypeElm.innerText = getRandomScale.type;
 };
 
 let changeScale: number;
